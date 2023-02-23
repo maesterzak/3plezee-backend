a56ef9b3adc9a569a712bbaeb8c6c8cc1ed7eb1b
maesterzak <abkdking1703@gmail.com>
2023-02-23 18:26:36 +0100

    increase token lifetime

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -203,8 +203,8 @@ SOCIALACCOUNT_PROVIDERS = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(hours=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=24),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True, 
     'UPDATE_LAST_LOGIN': True,
